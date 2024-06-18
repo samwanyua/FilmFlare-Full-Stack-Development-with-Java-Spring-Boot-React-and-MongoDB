27939ba129ba255a8af4e06145d7cc0636c69686
samwanyua <samexample8@gmail.com>
2024-06-18 12:13:22 +0300

    Create Review for a given movie

--- movie /src/main/java/com/filmflare/movie/Service/ReviewService.java
+++ movie /src/main/java/com/filmflare/movie/Service/ReviewService.java
@@ -1,20 +1,27 @@
 package com.filmflare.movie.Service;
 
+import com.filmflare.movie.Models.Movie;
 import com.filmflare.movie.Models.Review;
 import com.filmflare.movie.Repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
+    @Autowired
     private MongoTemplate mongoTemplate;
 
     public Review createReview(String reviewBody, String imdbId){
-        Review review = new Review(reviewBody);
-        reviewRepository.insert(review);
-
+        Review review = reviewRepository.insert(new Review(reviewBody));
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+        return review;
     }
 }
