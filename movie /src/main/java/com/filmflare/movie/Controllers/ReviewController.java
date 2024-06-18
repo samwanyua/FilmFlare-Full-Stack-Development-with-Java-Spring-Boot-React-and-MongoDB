8b6beb4d28d449d7847ff0b70b2ecd4c1b92bb99
samwanyua <samexample8@gmail.com>
2024-06-18 12:39:14 +0300

    Create Review for a given movie

--- movie /src/main/java/com/filmflare/movie/Controllers/ReviewController.java
+++ movie /src/main/java/com/filmflare/movie/Controllers/ReviewController.java
@@ -5,15 +5,13 @@ import com.filmflare.movie.Service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/movies")
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
--- movie /src/main/java/com/filmflare/movie/Service/ReviewService.java
+++ movie /src/main/java/com/filmflare/movie/Service/ReviewService.java
@@ -6,8 +6,11 @@ import com.filmflare.movie.Repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReviewService {
