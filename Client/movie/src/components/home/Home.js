1292368cdf3190df6a23175774e2edebefd76666
samwanyua <samexample8@gmail.com>
2024-06-18 19:58:01 +0300

    Add hero section

--- Client/movie/src/components/home/Home.js
+++ Client/movie/src/components/home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Hero from '../hero/Hero'
 
-const Home = () => {
+const Home = ({movies}) => {
   return (
-    <div>Home</div>
+    <Hero movies={movies}/>
   )
 }
 
